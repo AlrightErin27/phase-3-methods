324e3390d5901749fc9e25e08317b8e089752aa9
Erin Van Brunt <erinmontybruce@gmail.com>
2022-04-05 14:21:07 -0700

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,22 @@
 # Your code here!
+
+def greet_programmer
+    print "Hello, programmer!"
+end
+
+def greet(name)
+    print "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    print "Hello, #{name}!"
+end
+
+def add(a, b)
+   a + b
+end
+
+def halve(a)
+    return nil unless a.class == Integer
+    a/2
+end
